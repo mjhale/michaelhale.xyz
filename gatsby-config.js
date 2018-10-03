70be383e60223e5e2f089fa37ed1d33f66bdbea1
Michael Hale <mj504b@gmail.com>
2018-10-02 21:40:27 -0400

    Initialize Gatsby starter project.

---
+++ gatsby-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png',
+      },
+    },
+    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+
+    pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+
+
+  ],
+}

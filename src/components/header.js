ab6dbf3058ec0d8569a7734c5ff9346e5562a515
Michael Hale <mail@michaelhale.org>
2019-03-09 23:46:30 -0500

    Update layout styles.

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ import { Link } from 'gatsby';
 import { Container } from './Container';
 
 const Header = styled.header`
-  background: #3C4F65;
+  background: #825a84;
 `;
 
 const HeaderContainer = styled(Container)`
@@ -27,7 +27,7 @@ const Logo = styled.h1`
 `;
 
 const LogoLink = styled(Link)`
-  color: #F5F3EC;
+  color: #f5f3ec;
   text-decoration: none;
 `;
 
@@ -36,6 +36,7 @@ const MenuToggle = styled.button`
 `;
 
 const NavItem = styled.li`
+  line-height: 1.4;
   margin-bottom: 0;
 
   &:not(:last-of-type) {
@@ -44,12 +45,21 @@ const NavItem = styled.li`
 `;
 
 const NavLink = styled(Link)`
-  color: #F5F3EC;
+  color: rgba(242, 238, 255, 0.7);
   font-family: 'Roboto', sans-serif;
+  font-size: 0.9rem;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
   font-weight: 400;
   letter-spacing: 0.07em;
   text-decoration: none;
   text-transform: lowercase;
+  text-rendering: optimizeLegibility;
+  transition: color 0.1s linear;
+
+  &:hover {
+    color: #fff;
+  }
 `;
 
 const NavList = styled.ul`
@@ -65,17 +75,23 @@ const HeaderComponent = ({ siteTitle }) => (
   <Header>
     <HeaderContainer>
       <Logo>
-        <LogoLink to="/">
-          {siteTitle}
-        </LogoLink>
+        <LogoLink to="/">{siteTitle}</LogoLink>
       </Logo>
       <nav aria-label="Main">
         <NavList>
           <MenuToggle aria-expanded="false">Menu</MenuToggle>
-          <NavItem><NavLink to="/portfolio/">Portfolio</NavLink></NavItem>
-          <NavItem><NavLink to="/services/">Services</NavLink></NavItem>
-          <NavItem><NavLink to="/about/">About</NavLink></NavItem>
-          <NavItem><NavLink to="/contact/">Contact</NavLink></NavItem>
+          <NavItem>
+            <NavLink to="/portfolio/">Portfolio</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/services/">Services</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/about/">About</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/contact/">Contact</NavLink>
+          </NavItem>
         </NavList>
       </nav>
     </HeaderContainer>
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
-import { createGlobalStyle } from "styled-components"
+import { createGlobalStyle } from 'styled-components';
 import { graphql, StaticQuery } from 'gatsby';
 
 import Header from './Header';
@@ -11,6 +11,13 @@ import { Container } from './Container';
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: #F5F3EC;
+    font-feature-settings: "pnum";
+    -webkit-font-feature-settings: "pnum";
+    font-variant-numeric: proportional-nums;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    text-rendering: optimizeLegibility;
+    -webkit-text-size-adjust: 100%;
   }
 `;
 
@@ -35,16 +42,12 @@ const Layout = ({ children }) => (
         <GlobalStyle />
         <Helmet
           title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-          ]}
+          meta={[{ name: 'description', content: 'Sample' }]}
         >
           <html lang="en" />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <ChildrenContainer>
-          {children}
-        </ChildrenContainer>
+        <ChildrenContainer>{children}</ChildrenContainer>
       </>
     )}
   />

0854f46d7fc6ccd54be1107006b44a4c82eca747
Michael Hale <mail@michaelhale.org>
2019-04-13 13:36:31 -0400

    Add responsive navigation with toggle.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,15 +8,19 @@ import { breakpoints, media } from 'src/utils/media';
 
 const StyledNavItem = styled.li`
   line-height: 1.4;
-  margin-bottom: 0;
+  margin-bottom: 1.5rem;
 
-  &:not(:last-of-type) {
-    margin-right: 1rem;
-  }
+  ${media.md`
+    margin-bottom: 0;
+
+    &:not(:last-of-type) {
+      margin-right: 1rem;
+    }
+  `}
 `;
 
 const StyledNavLink = styled(Link)`
-  color: rgba(242, 238, 255, 0.93);
+  display: block;
   font-display: swap;
   font-family: 'Roboto', sans-serif;
   font-size: 0.9rem;
@@ -25,71 +29,152 @@ const StyledNavLink = styled(Link)`
   font-weight: 400;
   letter-spacing: 0.07em;
   text-decoration: none;
-  text-transform: lowercase;
   text-rendering: optimizeLegibility;
   transition: color 0.1s linear;
 
-  &:hover {
-    color: #fff;
+  @media (max-width: ${breakpoints.md}px) {
+    border-bottom: 1px solid rgb(219, 219, 219);
+    color: rgb(72, 72, 72);
+    padding-bottom: 1rem;
+    padding-top: 1rem;
   }
+
+  ${media.md`
+    color: rgba(242, 238, 255, 0.93);
+    text-transform: lowercase;
+
+    &:hover {
+      color: #fff;
+    }
+  `}
 `;
 
 const StyledNavList = styled.ul`
-  display: ${props => (props.isExpanded ? 'flex' : 'none')};
+  display: ${props => (props.isNavExpanded ? 'flex' : 'none')};
+  height: 100%;
   list-style: none;
   margin: 0;
+
+  @media (max-width: ${breakpoints.md}px) {
+    background-color: #f5f3ec;
+    flex-direction: column;
+    left: 0;
+    padding: 2.25rem;
+    position: absolute;
+    top: calc(5rem + 10px);
+    width: 100%;
+    z-index: 20;
+  }
 
   ${media.md`
     align-items: center;
     align-self: flex-end;
-    display: flex;
     flex-grow: 1;
   `}
 `;
 
 const StyledNavToggle = styled.button`
-  display: ${props => (props.isExpanded ? 'none' : 'inline-block')};
+  @media (max-width: ${breakpoints.md}px) {
+    background: none;
+    border: none;
+    cursor: pointer;
+    display: block;
+    height: 2rem;
+    opacity: 0.8;
+    padding: 0 0.5rem;
+    position: relative;
+    width: 2.15rem;
+
+    &:hover,
+    &:focus {
+      opacity: 1;
+    }
+  }
 
   ${media.md`
     display: none;
   `}
 `;
 
-const NavToggle = ({ isExpanded, setIsExpanded }) => {
+const StyledNavToggleText = styled.span`
+  left: 0.6rem;
+  position: absolute;
+  top: 1rem;
+  transform: ${props => (!props.isNavExpanded ? 'none' : 'rotate(45deg)')};
+  transition-duration: 0.1s;
+  transition-timing-function: cubic-bezier(0.5, 0.05, 0.7, 0.2);
+
+  &,
+  &::after,
+  &::before {
+    background-color: #fff;
+    display: block;
+    height: 2px;
+    transition: ${props =>
+      !props.isNavExpanded ? 'none' : 'transform 1s ease'};
+    transition-timing-function: ${props =>
+      !props.isNavExpanded ? 'none' : 'cubic-bezier(0.2, 0.6, 0.4, 1)'};
+    width: 1rem;
+  }
+
+  &::after,
+  &::before {
+    content: '';
+    position: absolute;
+  }
+
+  &::after {
+    bottom: ${props => (!props.isNavExpanded ? '0.35rem' : '0')};
+    transform: ${props => (!props.isNavExpanded ? 'none' : 'rotate(-90deg)')};
+    transition: bottom 0.1s 0.2s ease,
+      transform 0.1s cubic-bezier(0.5, 0.05, 0.7, 0.2);
+  }
+
+  &::before {
+    top: ${props => (!props.isNavExpanded ? '0.35rem' : '0')};
+    transition: top 0.1s 0.2s ease, opacity 0.1s ease;
+  }
+`;
+
+const NavToggle = ({ isNavExpanded, setisNavExpanded }) => {
   return (
     <StyledNavToggle
-      aria-expanded={isExpanded}
-      isExpanded={isExpanded}
-      onClick={() => setIsExpanded(true)}
+      aria-controls="nav-list"
+      aria-expanded={isNavExpanded}
+      isNavExpanded={isNavExpanded}
+      onClick={() => setisNavExpanded(!isNavExpanded)}
     >
-      Menu
+      <StyledNavToggleText isNavExpanded={isNavExpanded} />
     </StyledNavToggle>
   );
 };
 
 NavToggle.propTypes = {
-  isExpanded: PropTypes.bool.isRequired,
-  setIsExpanded: PropTypes.func.isRequired,
+  isNavExpanded: PropTypes.bool,
+  setisNavExpanded: PropTypes.func.isRequired,
 };
 
 const Nav = () => {
-  const [isExpanded, setIsExpanded] = useState(null);
+  const [isNavExpanded, setisNavExpanded] = useState(null);
 
   let windowWidth = useWindowWidth();
-  let isSmallDevice = windowWidth <= breakpoints.sm;
+  let isSmallDevice = windowWidth < breakpoints.md;
 
   useEffect(() => {
     if (isSmallDevice) {
-      setIsExpanded(false);
+      setisNavExpanded(false);
     } else {
-      setIsExpanded(true);
+      setisNavExpanded(true);
     }
   }, [isSmallDevice]);
 
   return (
     <nav aria-label="Main">
-      <NavToggle isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
-      <StyledNavList isExpanded={isExpanded}>
+      <NavToggle
+        isNavExpanded={isNavExpanded}
+        setisNavExpanded={setisNavExpanded}
+      />
+      <StyledNavList id="nav-list" isNavExpanded={isNavExpanded}>
         <StyledNavItem>
           <StyledNavLink to="/work/">Work</StyledNavLink>
         </StyledNavItem>
